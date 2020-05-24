d290b9bd4f2607df0a86e3a1690050b94af9692c
Shitij <shitij15@gmail.com>
2020-05-24 17:53:49 +0530

    second

---
+++ photos.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env sh
+for file in segregate/*.jpg; do
+	name=${file##*/}
+	year=$(echo $name | cut -d '-' -f 1)
+	month=$(echo $name | cut -d '-' -f 2)
+	if [ ! -d "$year/$month" ]; then
+		mkdir -p $year/$month
+		mv segregate_photos/$name $year/$month/$name
+	else 
+		mv segregate_photos/$name $year/$month/$name
+	fi
+done
